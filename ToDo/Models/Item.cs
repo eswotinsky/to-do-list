d88135880869d389edf4b8e2f3a7effc8a53793a
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-02-28 10:35:17 -0800

    refactor GetItems() and GetCategories() methods for MySql efficiency.

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -189,7 +189,7 @@ namespace ToDoList.Models
                 conn.Close();
             }
         }
-        
+
         public void AddCategory(Category newCategory)
         {
             MySqlConnection conn = DB.Connection();
@@ -219,44 +219,26 @@ namespace ToDoList.Models
         {
             MySqlConnection conn = DB.Connection();
             conn.Open();
-            var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"SELECT category_id FROM categories_items WHERE item_id = @itemId;";
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT categories.* FROM items
+              JOIN categories_items ON (items.id = categories_items.item_id)
+              JOIN categories ON (categories_items.category_id = categories.id)
+              WHERE items.id = @itemId;";
 
             MySqlParameter itemIdParameter = new MySqlParameter();
             itemIdParameter.ParameterName = "@itemId";
             itemIdParameter.Value = _id;
             cmd.Parameters.Add(itemIdParameter);
 
-            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
 
-            List<int> categoryIds = new List<int> {};
+            List<Category> categories = new List<Category> {};
             while(rdr.Read())
             {
                 int categoryId = rdr.GetInt32(0);
-                categoryIds.Add(categoryId);
-            }
-            rdr.Dispose();
-
-            List<Category> categories = new List<Category> {};
-            foreach (int categoryId in categoryIds)
-            {
-                var categoryQuery = conn.CreateCommand() as MySqlCommand;
-                categoryQuery.CommandText = @"SELECT * FROM categories WHERE id = @CategoryId;";
-
-                MySqlParameter categoryIdParameter = new MySqlParameter();
-                categoryIdParameter.ParameterName = "@CategoryId";
-                categoryIdParameter.Value = categoryId;
-                categoryQuery.Parameters.Add(categoryIdParameter);
-
-                var categoryQueryRdr = categoryQuery.ExecuteReader() as MySqlDataReader;
-                while(categoryQueryRdr.Read())
-                {
-                    int thisCategoryId = categoryQueryRdr.GetInt32(0);
-                    string categoryName = categoryQueryRdr.GetString(1);
-                    Category foundCategory = new Category(categoryName, thisCategoryId);
-                    categories.Add(foundCategory);
-                }
-                categoryQueryRdr.Dispose();
+                string categoryName = rdr.GetString(1);
+                Category newCategory = new Category(categoryName, categoryId);
+                categories.Add(newCategory);
             }
             conn.Close();
             if (conn != null)
